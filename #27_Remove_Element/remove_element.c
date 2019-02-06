d13ba71d3d5b358774cba808b8c1eff2f7dd642d
Oscar Hsu <vm3y3rmp40719@gmail.com>
2019-02-06 15:58:35 +0800

    Initial commit

---
+++ #27_Remove_Element/remove_element.c
@@ -0,0 +1,17 @@
+int removeElement(int nums, int numsSize, int val)
+{
+    int len = numsSize;
+
+    int i = 0;
+    int j;
+    for (j = 0; j len; j++)
+    {
+        if (nums[j] != val)
+        {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+
+    return i;
+}
